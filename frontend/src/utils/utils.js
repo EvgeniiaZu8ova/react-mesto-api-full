c6fc827756b6a34a76314ee204339d9d65a6c27a
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-06-18 18:33:11 +0400

    fix: поменять localhost на домен на фронте

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,7 @@
 // Объект настроек для экземпляров классов Api и Auth
 export const options = {
-  // url: "http://api.evzu8ova.students.nomoredomains.club",
-  url: "http://localhost:3000",
+  url: "http://api.evzu8ova.students.nomoredomains.club",
+  // url: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
