dcf7d83d668b295fb594f65e789114c80bd8e6b3
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-06-18 19:43:13 +0400

    fix: убрать лишние комментарии в utils фронтенда

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,7 +1,6 @@
 // Объект настроек для экземпляров классов Api и Auth
 export const options = {
   url: "http://api.evzu8ova.students.nomoredomains.club",
-  // url: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
